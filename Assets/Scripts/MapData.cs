0bfec3a06e5640c588e5c58f45f3dcbcade5d718
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-07-08 11:58:12 -0700

    First commit.

---
+++ Assets/Scripts/MapData.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MapData : MonoBehaviour
+{
+    public int width = 10;
+    public int height = 5;
+    public TextAsset textAsset;
+    public Texture2D textureMap;
+    public string resourcePath = "Mapdata";
+
+    private void Awake()
+    {
+        string levelName = SceneManager.GetActiveScene().name;
+        if (textureMap == null)
+        {
+            //textureMap = Resources.Load(resourcePath + "/" + levelName) as Texture2D;
+
+            //Texture2D texture = new Texture2D(2, 2);
+            textureMap = Resources.Load<Texture2D>(resourcePath + "/" + levelName);
+            //textureMap = xx;
+        }
+
+        if (textAsset == null)
+        {
+            textAsset = Resources.Load(resourcePath + "/" + levelName) as TextAsset;
+        }
+    }
+    public List<string> GetMapFromTextFile(TextAsset textAsset)
+    {
+        List<string> lines = new List<string>();
+        if (textAsset != null)
+        {
+            string textData = textAsset.text;
+            string[] delimiters = { "\r\n", "\n" };
+            lines.AddRange(textData.Split(delimiters, System.StringSplitOptions.None));
+            lines.Reverse();
+        }
+
+        return lines;
+    }
+
+    public List<string> GetMapFromTextFile()
+    {
+        return GetMapFromTextFile(textAsset);
+    }
+
+    public List<string> GetMapFromTexture(Texture2D texture)
+    {
+        List<string> lines = new List<string>();
+        
+        if(texture != null)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                string newLine = "";
+                for (int x = 0; x < texture.width; x++)
+                {
+                    if (texture.GetPixel(x, y) == Color.black)
+                    {
+                        newLine += '1';
+                    }
+                    else if (texture.GetPixel(x, y) == Color.white)
+                    {
+                        newLine += '0';
+                    }
+                    else
+                    {
+                        newLine += ' ';
+                    }
+                }
+                lines.Add(newLine);
+            }
+        }
+
+        return lines;
+    }
+
+    public void SetDimensions(List<string> textLines)
+    {
+        height = textLines.Count;
+        foreach (string line in textLines)
+        {
+            if(line.Length > width)
+            {
+                width = line.Length;
+            }
+        }
+    }
+
+    public int[,] MakeMap()
+    {
+        List<string> lines = new List<string>();
+        if(textureMap != null)
+        {
+            lines = GetMapFromTexture(textureMap);
+        }
+        else
+        {
+            lines = GetMapFromTextFile(textAsset);
+        }
+
+        SetDimensions(lines);
+
+        int[,] map = new int[width, height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (lines[y].Length > x)
+                {
+                    map[x, y] = (int)char.GetNumericValue(lines[y][x]);
+                }
+            }
+        }
+
+        return map;
+    }
+}
---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class Pathfinder : MonoBehaviour
+{
+    Node m_startNode;
+    Node m_goalNode;
+    Graph m_graph;
+    GraphView m_graphView;
+
+    Queue<Node> m_frontierNodes;
+    List<Node> m_exploreNodes;
+    List<Node> m_pathNodes;
+
+    public Color startColor = Color.green;
+    public Color goalColor = Color.red;
+    public Color frontierColor = Color.magenta;
+    public Color exploredColor = Color.gray;
+    public Color pathColor = Color.cyan;
+    public Color arrowColor = new Color(216, 216, 216, 255);
+    public Color highLightColor = new Color(255, 255, 128, 255);
+
+    public bool showIterations = true;
+    public bool showColors = true;
+    public bool showArrows = true;
+    public bool exitOnGoal = true;
+
+    public bool isComplete = false;
+    int m_iterations = 0;
+
+    public enum Mode
+    {
+        BFS = 0,
+        Dijkstra = 1
+    }
+    public Mode mode = Mode.BFS;
+
+    public void Init(Graph graph, GraphView graphView, Node start, Node goal)
+    {
+        if (start == null || goal == null || graph == null || graphView == null)
+        {
+            Debug.LogWarning("PATHFINDER Init error: missing component(s)!");
+            return;
+        }
+
+        if (start.nodeType == NodeType.Blocked || goal.nodeType == NodeType.Blocked)
+        {
+            Debug.LogWarning("PATHFINDER Init error: start and goal nodes must be unblocked!");
+            return;
+        }
+
+        m_graph = graph;
+        m_graphView = graphView;
+        m_startNode = start;
+        m_goalNode = goal;
+        ShowColors(graphView, start, goal);
+
+        m_frontierNodes = new Queue<Node>();
+        m_frontierNodes.Enqueue(start);
+        m_exploreNodes = new List<Node>();
+        m_pathNodes = new List<Node>();
+
+        for (int x = 0; x < m_graph.Width; x++)
+        {
+            for (int y = 0; y < m_graph.Height; y++)
+            {
+                m_graph.nodes[x, y].Reset();
+            }
+        }
+
+        isComplete = false;
+        m_iterations = 0;
+        m_startNode.distanceTraveled = 0;
+    }
+
+    void ShowColor()
+    {
+        ShowColors(m_graphView, m_startNode, m_goalNode);
+    }
+
+    private void ShowColors(GraphView graphView, Node start, Node goal)
+    {
+        if (graphView == null || start == null || goal == null) return;
+
+        if(m_frontierNodes != null)
+        {
+            graphView.ColorNodes(m_frontierNodes.ToList(), frontierColor);
+        }
+
+        if(m_exploreNodes != null)
+        {
+            graphView.ColorNodes(m_exploreNodes, exploredColor);
+        }
+
+        if(m_pathNodes != null && m_pathNodes.Count > 0)
+        {
+            graphView.ColorNodes(m_pathNodes, pathColor);
+        }
+
+        NodeView startNodeView = graphView.nodeViews[start.xIndex, start.yIndex];
+        if (startNodeView != null)
+        {
+            startNodeView.ColorNode(startColor);
+        }
+
+        NodeView goalNodeView = graphView.nodeViews[goal.xIndex, goal.yIndex];
+        if (goalNodeView != null)
+        {
+            goalNodeView.ColorNode(goalColor);
+        }
+    }
+
+    public IEnumerator SearchRoutine(float timeStep = 0.1f)
+    {
+        float timeStart = Time.time;
+        yield return null;
+
+        while (!isComplete)
+        {
+            if (m_frontierNodes.Count > 0)
+            {
+                Node currentNode = m_frontierNodes.Dequeue();
+                m_iterations++;
+
+                if (!m_exploreNodes.Contains(currentNode))
+                {
+                    m_exploreNodes.Add(currentNode);
+                }
+
+                if(mode == Mode.BFS)
+                {
+                    ExpandFrontierBreadthFirst(currentNode);
+                }
+                else if (mode == Mode.Dijkstra)
+                {
+                    ExpandFrontierDijkstra(currentNode);
+                }
+
+                if (m_frontierNodes.Contains(m_goalNode))
+                {
+                    m_pathNodes = GetPathNodes(m_goalNode);
+                    if (exitOnGoal)
+                    {
+                        isComplete = true;
+                        Debug.Log("PATHFINDER mode: " + mode.ToString() + " path length = " + m_goalNode.distanceTraveled.ToString());
+
+                    }
+                }
+
+                if (showIterations)
+                {
+                    ShowDiagnostics();
+                    yield return new WaitForSeconds(timeStep);
+                }
+            }
+            else
+            {
+                isComplete = true;
+            }
+        }
+
+        ShowDiagnostics();
+        Debug.Log("PATHFINDER SearchRoutine: elapse time = " + (Time.time - timeStart).ToString() + " seconds");
+    }
+
+    private void ShowDiagnostics()
+    {
+        if (showColors)
+        {
+            ShowColor();
+        }
+
+        if (m_graphView != null && showArrows)
+        {
+            m_graphView.ShowNodeArrows(m_frontierNodes.ToList(), arrowColor);
+
+            if (m_frontierNodes.Contains(m_goalNode))
+            {
+                m_graphView.ShowNodeArrows(m_pathNodes, highLightColor);
+            }
+        }
+    }
+
+    void ExpandFrontierBreadthFirst(Node node)
+    {
+        if (node != null)
+        {
+            for (int i = 0; i < node.neighbors.Count; i++)
+            {
+                if (!m_exploreNodes.Contains(node.neighbors[i]) && !m_frontierNodes.Contains(node.neighbors[i]))
+                {
+                    float distanceToNeighbor = m_graph.GetNodeDistance(node, node.neighbors[i]);
+                    float newDistanceTraveled = distanceToNeighbor + node.distanceTraveled;
+                    node.neighbors[i].distanceTraveled = newDistanceTraveled;
+
+                    node.neighbors[i].previous = node;
+                    m_frontierNodes.Enqueue(node.neighbors[i]);
+                }
+            }
+        }
+    }
+
+    void ExpandFrontierDijkstra(Node node)
+    {
+        if (node != null)
+        {
+            for (int i = 0; i < node.neighbors.Count; i++)
+            {
+                if (!m_exploreNodes.Contains(node.neighbors[i]))
+                {
+                    float distanceToNeighbor = m_graph.GetNodeDistance(node, node.neighbors[i]);
+                    float newDistanceTraveled = distanceToNeighbor + node.distanceTraveled;
+
+                    if(float.IsPositiveInfinity(node.neighbors[i].distanceTraveled) || newDistanceTraveled < node.neighbors[i].distanceTraveled)
+                    {
+                        node.neighbors[i].previous = node;
+                        node.neighbors[i].distanceTraveled = newDistanceTraveled;
+                    }
+
+                    if (!m_frontierNodes.Contains(node.neighbors[i]))
+                    {
+                        m_frontierNodes.Enqueue(node.neighbors[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Node> GetPathNodes(Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        if(endNode == null)
+        {
+            return path;
+        }
+        path.Add(endNode);
+
+        Node currentNode = endNode.previous;
+        while(currentNode != null)
+        {
+            path.Insert(0, currentNode);
+            currentNode = currentNode.previous;
+        }
+
+        return path;
+    }
+}
